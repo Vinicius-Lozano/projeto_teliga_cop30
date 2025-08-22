c7ab9f0c8ec2c984500d38e5442c68a7628db457
Fernando <nando.nicolau27@gmail.com>
2025-08-21 21:20:34 -0300

    add event de models.

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class events(models.Model):
+    titulos = models.CharField(max_length=200)
+    descricao = models.TextField()
+    data = models.DateTimeField()
+    tipo = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    class Meta:
+        ordering = ['data'] #lista primeiro o mais recente 
+
+    def __str__(self):
+        return f'Título: {self.titulos} | Tipo: {self.tipo} | Data: {self.data} | Localização: ({self.latitude}, {self.longitude})'
